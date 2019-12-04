e987311af9d0f75d0a4a862c71c73988bdebfad2
Kate-CCRi <ksecor@ccri.com>
2019-12-03 20:42:16 -0500

    Part 2 rewritten to be more pythonic, solution works

--- Day 02/Day2_Part2.py
+++ Day 02/Day2_Part2.py
@@ -1,3 +1,6 @@
+# Import the array functions
+import array
+
 # Open the input file, read it into the "data" variable, and clean it up
 f = open("input.txt", "r")
 data = f.read()
@@ -10,48 +13,41 @@ nums = data.split(",")
 
 nums = list(map(int, nums))
 
+# Update the values in the array to what the problem asks for
+
 nums[1] = 12
 nums[2] = 2
 
-pos_place = 0
-first_op_place = 1
-second_op_place = 2
-location_place = 3
+def process_array(nums):
 
-while True:
+    # The working array "arr" is all of "nums" from start to finish
+    arr = nums[:]
 
-    if nums[pos_place] == 1:
-        op1_loc = nums[first_op_place]
-        op2_loc = nums[second_op_place]
-        op = nums[op1_loc] + nums[op2_loc]
-        loc = nums[location_place]
-        nums[loc] = op
-        pos_place += 4
-        first_op_place += 4
-        second_op_place += 4
-        location_place += 4
-        print(nums)
+   # Iterate over the working array by 4s:
+    for index in range(0, len(arr), 4):
+        operator = arr[index]
+        numberA = arr[arr[index+1]]
+        numberB = arr[arr[index+2]]
+        if operator == 99:
+            return arr[0]
+        elif operator == 1:
+            arr[arr[index+3]] = numberA + numberB
+        elif operator == 2:
+            arr[arr[index+3]] = numberA * numberB
 
-    elif nums[pos_place] == 2:
-        op1_loc = nums[first_op_place]
-        op2_loc = nums[second_op_place]
-        op = nums[op1_loc] * nums[op2_loc]
-        loc = nums[location_place]
-        nums[loc] = op
-        pos_place += 4
-        first_op_place += 4
-        second_op_place += 4
-        location_place += 4
-        print(nums)
+    return arr[0]
 
-    elif nums[pos_place] == 99:
-        break
+# Part 2 specific
 
-    else:
-        print("I found a number that doesn't work!")
+# Range takes an argument that is one higher than where you want it to stop
+for noun in range(100):
+    # Nesting it this way makes it check each noun against each verb
+    for verb in range(100):
+        nums[1] = noun
+        nums[2] = verb
 
-print(nums)
+        output = process_array(nums)
 
-
-
-
+        if output == 19690720:
+            print(100 * noun + verb)
+            break
